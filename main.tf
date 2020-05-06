fd11ab9345deb4af761d5bd0a188e627bc251c67
phaktor <panxer3@gmail.com>
2020-05-06 00:53:58 +0000

    removed comment

--- main.tf
+++ main.tf
@@ -5,13 +5,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-#https://learn.hashicorp.com/terraform/getting-started/install.html - to install terraform
-#type 'terraform init' before you start.
-#aws_access_key_id = "type your IAM access key id to here"
-#aws_secret_access_key = "type your IAM secret access key to here"
-#Put above two lines to secret-variables.auto.tfvars file.
-#terraform apply to build the app.
-
 module "VPC" {
   source = "./VPC"
   PublicSubnet = module.VPC.PublicSubnet
